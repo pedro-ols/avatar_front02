2d7a85659a2d0975d34002ed0e452c378164ba69
Pedro Santos <pedro.o.santos7@aluno.senai.br>
2025-09-23 16:22:25 -0300

    feat: create navbar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,32 @@
 import styles from "./header.module.css";
+import Link from "next/link"
 
 export default function Header() {
   return (
     <header className={styles.header}>
         <h1 className={styles.title}>🔥🌊Avatar - API front-End🌱💨</h1>
 
-        <nav>
-            <div className={styles.navItem}></div>
+        <nav className={styles.navBar}>
+            <div className={styles.navItem}>
+                <Link href="/get" className={styles.navLink}>
+                    Personagens
+                </Link>
+            </div>
+            <div className={styles.navItem}>
+                <Link href="/getById" className={styles.navLink}>
+                    Buscar Personagens por Id
+                </Link>
+            </div>
+            <div  className={styles.navItem}>
+                <Link href="/create" className={styles.navLink}>
+                    Criar Personagem
+                </Link>
+            </div>
+            <div className={styles.navItem}>
+                <Link href="/api" className={styles.navLink}>
+                    API
+                </Link>
+            </div>
         </nav>
     </header>
   );
